d88e8177da0cbfe9eecb4e1f976b95e5f5a7b1cc
Kieran W <41634689+FredHappyface@users.noreply.github.com>
2019-03-11 12:25:27 +0000

    Update Go.Sqareroot.go

--- Go.Sqareroot.go
+++ Go.Sqareroot.go
@@ -1,6 +1,11 @@
 //go 1.10.4
 
+// Copyright 2019 Kieran W.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package main
+
 import "fmt"
 
 func sqrt(x float64) float64 {
